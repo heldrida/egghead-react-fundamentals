26692862e78e768e7a2bc67c9cd2813c8abecc56
Helder <helder@lamoulade.com>
2016-03-21 17:28:56 +0000

    set default props

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 class App extends React.Component {
 	render () {
-		return <div>Hello world!</div>
-	}
-}
+		let txt = this.props.txt;
+		return <h1>{txt}</h1>
+	};
+};
 
-export default App;
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+};
+
+App.defaultProps = {
+	txt: 'This is the default txt'
+};
+
+ReactDOM.render(
+	<App cat={5} />,
+	document.getElementById('app')
+);
